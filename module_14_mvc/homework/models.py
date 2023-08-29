e449ba32069ba655814a94082b1841296e07cd94
Bekkhan Israpilov <israpal@bk.ru>
2023-08-29 18:33:10 +0300

    added module-14 homework

--- module_14_mvc/homework/models.py
+++ module_14_mvc/homework/models.py
@@ -1,19 +1,20 @@
 import sqlite3
-from typing import Any, Optional, List
+from typing import Any, Optional, List, Tuple
 
 DATA: List[dict] = [
-    {'id': 0, 'title': 'A Byte of Python', 'author': 'Swaroop C. H.'},
-    {'id': 1, 'title': 'Moby-Dick; or, The Whale', 'author': 'Herman Melville'},
-    {'id': 3, 'title': 'War and Peace', 'author': 'Leo Tolstoy'},
+    {'id': 0, 'title': 'A Byte of Python', 'author': 'Swaroop C. H.', 'counter': 0},
+    {'id': 1, 'title': 'Moby-Dick; or, The Whale', 'author': 'Herman Melville', 'counter': 0},
+    {'id': 3, 'title': 'War and Peace', 'author': 'Leo Tolstoy', 'counter': 0},
 ]
 
 
 class Book:
 
-    def __init__(self, id: int, title: str, author: str) -> None:
+    def __init__(self, id: int, title: str, author: str, counter: int) -> None:
         self.id: int = id
         self.title: str = title
         self.author: str = author
+        self.counter: int = counter
 
     def __getitem__(self, item: str) -> Any:
         return getattr(self, item)
@@ -36,17 +37,18 @@ def init_db(initial_records: List[dict]) -> None:
                 CREATE TABLE `table_books` (
                     id INTEGER PRIMARY KEY AUTOINCREMENT, 
                     title TEXT, 
-                    author TEXT, 
+                    author TEXT,
+                    counter INTEGER
                 )
                 """
             )
             cursor.executemany(
                 """
                 INSERT INTO `table_books`
-                (title, author) VALUES (?, ?)
+                (title, author, counter) VALUES (?, ?, ?)
                 """,
                 [
-                    (item['title'], item['author'])
+                    (item['title'], item['author'], item['counter'])
                     for item in initial_records
                 ]
             )
@@ -60,4 +62,86 @@ def get_all_books() -> List[Book]:
             SELECT * from `table_books`
             """
         )
-        return [Book(*row) for row in cursor.fetchall()]
+        all_books = cursor.fetchall()
+
+        if all_books:
+            counter_up(cursor, all_books)
+
+        return [Book(*row) for row in all_books]
+
+
+def add_new_book(title: str, author: str) -> None:
+    with sqlite3.connect('table_books.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        cursor.execute(
+            """
+            INSERT INTO 'table_books'(title, author)
+                VALUES (?, ?)
+            """,
+            (title, author)
+        )
+
+
+def get_authors_books(author: str) -> List[tuple]:
+    with sqlite3.connect('table_books.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        cursor.execute(
+            """
+            SELECT title, author FROM table_books
+                WHERE author LIKE ?
+            """,
+            ('%' + author + '%', )
+        )
+        authors_books = cursor.fetchall()
+
+        if authors_books:
+            counter_up(cursor, authors_books)
+
+        return authors_books
+
+
+def counter_up(cursor: sqlite3.Cursor, books_list: List) -> None:
+
+    if len(books_list[0]) == 4:
+        books_id = [{'id': book[0]} for book in books_list]
+        cursor.executemany(
+            """
+            UPDATE table_books
+                SET counter = (
+                        SELECT counter FROM table_books
+                        WHERE id = :id
+                        ) + 1
+                WHERE id = :id
+            """,
+            books_id
+        )
+    else:
+        books_id = [{'title': book[0]} for book in books_list]
+        cursor.executemany(
+            """
+            UPDATE table_books
+                SET counter = (
+                        SELECT counter FROM table_books
+                        WHERE title = :title
+                        ) + 1
+                WHERE title = :title
+            """,
+            books_id
+        )
+
+
+def get_book_info(book_id: int) -> Tuple:
+    with sqlite3.connect('table_books.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        cursor.execute(
+            """
+            SELECT * FROM table_books
+                WHERE id = ?
+            """,
+            (book_id, )
+        )
+        book_info = cursor.fetchall()
+
+        if book_info:
+            counter_up(cursor, book_info)
+            return book_info[0]

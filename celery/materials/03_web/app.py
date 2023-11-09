b3df9ce1e18e4d3ca2f7f31b4764ad4a4aa127a6
Bekkhan Israpilov <israpal@bk.ru>
2023-11-09 14:29:58 +0300

    added module-22 homework

--- celery/materials/03_web/app.py
+++ celery/materials/03_web/app.py
@@ -1,13 +1,19 @@
 import random
+import signal
 
 from flask import Flask, request, jsonify
 from celery import Celery, group
+
 import time
 
 app = Flask(__name__)
 
+TASKS = None
+QUEUES = None
+
+
 # Конфигурация Celery
-celery = Celery(
+celery_app = Celery(
     app.name,
     broker='redis://localhost:6379/0',
     backend='redis://localhost:6379/0',
@@ -15,12 +21,24 @@ celery = Celery(
 
 
 # Задача Celery для обработки изображения
-@celery.task
+@celery_app.task
 def process_image(image_id):
     # В реальной ситуации здесь может быть обработка изображения
     # В данном примере просто делаем задержку для демонстрации
     time.sleep(random.randint(5, 15))
     return f'Image {image_id} processed'
+
+
+@celery_app.on_after_configure.connect
+def periodic_status_checker(sender, **kwargs):
+    sender.add_periodic_task(10, tasks_queues_status.s())
+
+
+@celery_app.task()
+def tasks_queues_status():
+    global TASKS, QUEUES
+    TASKS = celery_app.control.inspect().active()
+    QUEUES = celery_app.control.inspect().active_queues()
 
 
 @app.route('/process_images', methods=['POST'])
@@ -33,7 +51,6 @@ def process_images():
             process_image.s(image_id)
             for image_id in images
         )
-
         # Запускаем группу задач и сохраняем ее
         result = task_group.apply_async()
         result.save()
@@ -46,16 +63,27 @@ def process_images():
 
 @app.route('/status/<group_id>', methods=['GET'])
 def get_group_status(group_id):
-    result = celery.GroupResult.restore(group_id)
+    result = celery_app.GroupResult.restore(group_id)
 
     if result:
         # Если группа с таким ID существует,
         # возвращаем долю выполненных задач
-        status = result.completed_count() / len(result)
+        status = str(round(result.completed_count() / len(result) * 100, 2)) + ' %'
         return jsonify({'status': status}), 200
     else:
         # Иначе возвращаем ошибку
         return jsonify({'error': 'Invalid group_id'}), 404
+
+
+@app.route('/cancel/<group_id>', methods=['GET'])
+def cancel_group_process(group_id):
+    celery_app.control.revoke(group_id)
+    return jsonify(group_id=group_id, status='canceled'), 200
+
+
+@app.route('/control', methods=['GET'])
+def control_group_process():
+    return jsonify(tasks=TASKS, queues=QUEUES), 200
 
 
 if __name__ == '__main__':

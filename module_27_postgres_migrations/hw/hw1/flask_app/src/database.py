30016e0fb53911283fd6ebeb7d1ea7511d5ab72f
Bekkhan Israpilov <israpal@bk.ru>
2024-01-19 12:44:34 +0300

    homework from module-27 task-1 fix added

--- module_27_postgres_migrations/hw/hw1/flask_app/src/database.py
+++ module_27_postgres_migrations/hw/hw1/flask_app/src/database.py
@@ -10,14 +10,17 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level='DEBUG')
 
 
-POSTGRES_LOGIN = os.getenv('POSTGRES_LOGIN')
+POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 
-if not all((POSTGRES_LOGIN, POSTGRES_PASSWORD)):
-    logger.error('Missing environment variables POSTGRES_LOGIN or POSTGRES_PASSWORD')
+logging.debug(f"User: {POSTGRES_USER}")
+logging.debug(f"Password: {POSTGRES_PASSWORD}")
+
+if not all((POSTGRES_USER, POSTGRES_PASSWORD)):
+    logger.error('Missing environment variables POSTGRES_USER or POSTGRES_PASSWORD')
     close_gunicorn()
 
-POSTGRES_URL = f'postgresql+psycopg2://{POSTGRES_LOGIN}:{POSTGRES_PASSWORD}@localhost'
+POSTGRES_URL = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost'
 
 engine = create_engine(POSTGRES_URL, echo=True)
 Session = sessionmaker(bind=engine)
